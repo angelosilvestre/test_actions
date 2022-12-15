db60188338de9b4ea9283feb918a93ce6417f0d1
Angelo Silvestre <angelosilvestre.ccp@gmail.com>
2022-12-15 10:39:53 -0300

    Update samples

---
+++ samples/sample1.dart
@@ -0,0 +1,17 @@
+/// TITLE: Multiple blocks sample.
+/// STEPS:
+/// 1: Declare the methods.
+/// 2: Implement the methods.
+//>step:1
+void method1() {
+//>step:2 Add this inside method1()
+  print('implementation');
+//<step:2
+}
+
+void method2() {
+//>step:2 Add this inside method2()
+  print('implementation');
+//<step:2
+}
+//<step:1
